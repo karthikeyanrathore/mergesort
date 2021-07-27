41706d9b269466d1463fe0ee77c323ee1e71f368
karthikeyanrathore <karthikerathore@gmail.com>
2021-07-27 23:26:31 +0530

    fun & clean

--- run.sh
+++ run.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 printf "  generating test cases \n  "
-g++ stresstest.cc && ./a.out  > docs/in
+g++ stresstest.cc && ./a.out  > test/in
 
 printf "  running brute.cc \n "
-g++ brute.cc && ./a.out  < docs/in > docs/brute
+g++ brute.cc && ./a.out  < test/in > test/brute
 
 printf "  running mergesort.cc \n "
-g++ mergesort.cc && ./a.out  < docs/in > docs/cpp
+g++ mergesort.cc && ./a.out  < test/in > test/cpp
 
 
 printf "  running mergesort.py \n "
 chmod +x mergesort.py
-./mergesort.py < docs/in > docs/py
+./mergesort.py < test/in > test/py
 
 printf "  running mergesort.go \n "
-go run  mergesort.go  < docs/in > docs/go
+go run  mergesort.go  < test/in > test/go
 
 printf "  running mergesort.pas \n  "
-fpc mergesort.pas && ./mergesort < docs/in > docs/pas
+fpc mergesort.pas && ./mergesort < test/in > test/pas
 
 
 
